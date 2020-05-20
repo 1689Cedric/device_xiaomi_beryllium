67f3db238ceec943f3c73424c2e36754decefec8
1689Cedric <42259564+1689Cedric@users.noreply.github.com>
2020-05-20 11:23:04 +0800

    Update and rename havoc_beryllium.mk to aosp_beryllium.mk

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -6,18 +6,15 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Havoc stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.havoc.maintainer=REIGNZ
 
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys"
 
